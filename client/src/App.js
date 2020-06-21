ec1cb4c08612fc646a2bd6fd3fc43d21b9139993
avamisola <58097632+avamisola@users.noreply.github.com>
2020-06-21 00:13:37 -0400

    clean up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import "./global.css";
-import Authenticated from "./components/Authenticated";
+import Header from "./components/Header";
 import Courses from './components/Courses';
 import CreateCourse from './components/CreateCourse';
-import CourseDetail from './components/CourseDetail.js';
-import Header from "./components/Header";
-import NotFound from "./components/NotFound";
 import UpdateCourse from "./components/UpdateCourse";
+import CourseDetail from './components/CourseDetail.js';
 import UserSignIn from "./components/UserSignIn";
-import UserSignOut from "./components/UserSignOut";
 import UserSignUp from "./components/UserSignUp";
+import UserSignOut from "./components/UserSignOut";
 import withContext from "./Context";
 
 //pass context to each component 
 const HeaderWithContext = withContext(Header);
-const AuthWithContext = withContext(Authenticated);
-const UserSignUpWithContext = withContext(UserSignUp);
-const UserSignInWithContext = withContext(UserSignIn);
-const UserSignOutWithContext = withContext(UserSignOut);
-const CreateWithContext = withContext(CreateCourse);
-const CourseDetailInWithContext = withContext(CourseDetail);
+const CreateCourseWithContext = withContext(CreateCourse);
 const UpdateCourseWithContext = withContext(UpdateCourse);
+const CourseDetailWithContext = withContext(CourseDetail);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignOutWithContext = withContext(UserSignOut);
 
 //set up routes to each component
 export default () => (
@@ -31,15 +28,13 @@ export default () => (
     <HeaderWithContext />
     <div>
       <Switch>
-      <Route exact path="/" component={Courses} />
-        <PrivateRoute path="/authenticated" component={AuthWithContext} />
-        <PrivateRoute path='/courses/create' component={CreateWithContext} />
+        <Route exact path="/" component={Courses} />
+        <PrivateRoute path='/courses/create' component={CreateCourseWithContext} />
         <PrivateRoute exact path='/courses/:id/update' component={UpdateCourseWithContext} /> 
-        <Route exact path='/courses/:id' component={CourseDetailInWithContext} /> 
+        <Route exact path='/courses/:id' component={CourseDetailWithContext} /> 
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
-        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
